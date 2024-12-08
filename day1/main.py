95e319ce2277f5847349d56b830ba3c4af96d270
MrLeonardDude <leopagnez@gmail.com>
2024-12-08 18:08:33 -0300

    chore: lint day1

--- day1/main.py
+++ day1/main.py
@@ -2,31 +2,30 @@ from math import fabs
 
 FILE_LINE_SEPARATOR = "   "
 
+
 class HistoricallySignificantLocationsFileRepository:
 
     def __init__(self, filename: str):
         self.filename = filename
 
     def get_locations(self) -> list[list[int]]:
-        locations_lists = [
-            [],
-            []
-        ]
+        locations_lists = [[], []]
 
-        with open(self.filename, 'r') as fd:
+        with open(self.filename, "r") as fd:
             for line in fd.readlines():
-                first_elem, second_elem = line.split(FILE_LINE_SEPARATOR)      
+                first_elem, second_elem = line.split(FILE_LINE_SEPARATOR)
                 locations_lists[0].append(int(first_elem))
                 locations_lists[1].append(int(second_elem))
 
         return locations_lists
+
 
 def main(file_repo):
     localtion_lists = file_repo.get_locations()
 
     localtion_lists[0].sort()
     localtion_lists[1].sort()
-    
+
     difference = 0
     size_list = len(localtion_lists[0])
     for i in range(0, size_list):
@@ -37,6 +36,7 @@ def main(file_repo):
 
     print(difference)
 
+
 if __name__ == "__main__":
-    file_repo = HistoricallySignificantLocationsFileRepository('input.txt')
+    file_repo = HistoricallySignificantLocationsFileRepository("input.txt")
     main(file_repo)
